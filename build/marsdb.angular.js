a2aa7406d0ceb0c166c7830d2601a03ae848db08
Artem Artemyev <viiv.c58@gmail.com>
2016-02-04 14:41:11 +0500

    0.0.7

--- build/marsdb.angular.js
+++ build/marsdb.angular.js
@@ -1,7 +1,9 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
 
@@ -9,12 +11,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.createCollectionDelegate = createCollectionDelegate;
-
-var _AngularCursorObservable = require('./AngularCursorObservable');
-
-var _AngularCursorObservable2 = _interopRequireDefault(_AngularCursorObservable);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -34,7 +30,7 @@ function createCollectionDelegate() {
    * for finds.
    */
 
-  var AngularCollectionDelegate = (function (_defaultDelegate2) {
+  var AngularCollectionDelegate = function (_defaultDelegate2) {
     _inherits(AngularCollectionDelegate, _defaultDelegate2);
 
     function AngularCollectionDelegate(db, options) {
@@ -85,25 +81,26 @@ function createCollectionDelegate() {
       value: function find(query) {
         var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
 
-        return new _AngularCursorObservable2.default(this.$q, this.db, query, options);
+        return _get(Object.getPrototypeOf(AngularCollectionDelegate.prototype), 'find', this).call(this, query, _extends({}, options, { _$q: this.$q }));
       }
     }]);
 
     return AngularCollectionDelegate;
-  })(_defaultDelegate);
+  }(_defaultDelegate);
 
   return AngularCollectionDelegate;
 }
-},{"./AngularCursorObservable":2,"marsdb":undefined}],2:[function(require,module,exports){
+},{"marsdb":undefined}],2:[function(require,module,exports){
 'use strict';
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.createCursor = createCursor;
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -111,105 +108,105 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var CursorObservable = typeof window !== 'undefined' && window.Mars ? window.Mars.CursorObservable : require('marsdb').CursorObservable;
+var Collection = typeof window !== 'undefined' && window.Mars ? window.Mars.Collection : require('marsdb').Collection;
 
-var AngularCursorObservable = exports.AngularCursorObservable = (function (_CursorObservable) {
-  _inherits(AngularCursorObservable, _CursorObservable);
+/**
+ * Function that creates a cursor class based on current default
+ * cursor class in a collection
+ * @return {Cursor}
+ */
+function createCursor() {
+  var _defaultCursor = Collection.defaultCursor();
 
-  function AngularCursorObservable($q) {
-    var _Object$getPrototypeO;
+  var AngularCursorObservable = function (_defaultCursor2) {
+    _inherits(AngularCursorObservable, _defaultCursor2);
 
-    _classCallCheck(this, AngularCursorObservable);
+    function AngularCursorObservable() {
+      _classCallCheck(this, AngularCursorObservable);
 
-    for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-      args[_key - 1] = arguments[_key];
+      return _possibleConstructorReturn(this, Object.getPrototypeOf(AngularCursorObservable).apply(this, arguments));
     }
 
-    var _this = _possibleConstructorReturn(this, (_Object$getPrototypeO = Object.getPrototypeOf(AngularCursorObservable)).call.apply(_Object$getPrototypeO, [this].concat(args)));
+    _createClass(AngularCursorObservable, [{
+      key: 'destroy',
 
-    _this.$q = $q;
-    return _this;
-  }
-
-  /**
-   * Stop observing given cursor, if passed and
-   * observing.
-   * It might be useful when you need to replace
-   * previously created request with another one
-   * (with different query, for example).
-   *
-   * @param  {CursorObservable} cursor
-   */
-
-  _createClass(AngularCursorObservable, [{
-    key: 'destroy',
-    value: function destroy(cursor) {
-      console.warn('DEPRECATED: `destroy` of cursor is deprecated! use `stopObservers` instead');
-      return this.stopObservers();
-    }
-
-    /**
-     * Original `observe` with one additional argument.
-     * Second argument, if passed, a $scope for tracking
-     * $destroy event and stopping observing when event
-     * emited.
-     *
-     * @param  {Function} fn
-     * @param  {Scope}   $scope
-     * @return {Stooper}
-     */
-
-  }, {
-    key: 'observe',
-    value: function observe(fn, $scope) {
-      var stopper = _get(Object.getPrototypeOf(AngularCursorObservable.prototype), 'observe', this).call(this, fn);
-      if ($scope) {
-        $scope.$on('$destroy', function () {
-          stopper.stop();
-        });
-      }
-      return stopper;
-    }
-  }, {
-    key: '_doUpdate',
-    value: function _doUpdate() {
-      var _get2;
-
-      for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
-        args[_key2] = arguments[_key2];
+      /**
+       * Stop observing given cursor, if passed and
+       * observing.
+       * It might be useful when you need to replace
+       * previously created request with another one
+       * (with different query, for example).
+       * @param  {CursorObservable} cursor
+       */
+      value: function destroy(cursor) {
+        console.warn('DEPRECATED: `destroy` of cursor is deprecated! use `stopObservers` instead');
+        return this.stopObservers();
       }
 
-      return this.$q.resolve((_get2 = _get(Object.getPrototypeOf(AngularCursorObservable.prototype), '_doUpdate', this)).call.apply(_get2, [this].concat(args)));
-    }
-  }, {
-    key: 'then',
-    value: function then() {
-      var _get3;
+      /**
+       * Original `observe` with one additional argument.
+       * Second argument, if passed, a $scope for tracking
+       * $destroy event and stopping observing when event
+       * emited.
+       * @param  {Function} fn
+       * @param  {Scope}   $scope
+       * @return {Stooper}
+       */
 
-      for (var _len3 = arguments.length, args = Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
-        args[_key3] = arguments[_key3];
+    }, {
+      key: 'observe',
+      value: function observe(fn, $scope) {
+        var stopper = _get(Object.getPrototypeOf(AngularCursorObservable.prototype), 'observe', this).call(this, fn);
+        if ($scope) {
+          $scope.$on('$destroy', function () {
+            stopper.stop();
+          });
+        }
+        return stopper;
       }
+    }, {
+      key: '_doUpdate',
+      value: function _doUpdate() {
+        var _get2;
 
-      return this.$q.resolve((_get3 = _get(Object.getPrototypeOf(AngularCursorObservable.prototype), 'then', this)).call.apply(_get3, [this].concat(args)));
-    }
-  }]);
+        for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+          args[_key] = arguments[_key];
+        }
+
+        return this.options._$q.resolve((_get2 = _get(Object.getPrototypeOf(AngularCursorObservable.prototype), '_doUpdate', this)).call.apply(_get2, [this].concat(args)));
+      }
+    }, {
+      key: 'then',
+      value: function then() {
+        var _get3;
+
+        for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+          args[_key2] = arguments[_key2];
+        }
+
+        return this.options._$q.resolve((_get3 = _get(Object.getPrototypeOf(AngularCursorObservable.prototype), 'then', this)).call.apply(_get3, [this].concat(args)));
+      }
+    }]);
+
+    return AngularCursorObservable;
+  }(_defaultCursor);
 
   return AngularCursorObservable;
-})(CursorObservable);
-
-exports.default = AngularCursorObservable;
+}
 },{"marsdb":undefined}],3:[function(require,module,exports){
 'use strict';
 
 var _AngularCollectionDelegate = require('./AngularCollectionDelegate');
+
+var _AngularCursorObservable = require('./AngularCursorObservable');
 
 var angular = typeof window !== 'undefined' && window.angular ? window.angular : require('angular');
 var Collection = typeof window !== 'undefined' && window.Mars ? window.Mars.Collection : require('marsdb').Collection;
 
 // Setup mars $collection provider
 angular.module('MarsDB', []).provider('$collection', function () {
-  var _angularCollectionDelegate = (0, _AngularCollectionDelegate.createCollectionDelegate)();
-  Collection.defaultDelegate(_angularCollectionDelegate);
+  Collection.defaultDelegate((0, _AngularCollectionDelegate.createCollectionDelegate)());
+  Collection.defaultCursor((0, _AngularCursorObservable.createCursor)());
 
   this.defaultDelegate = function (delegate) {
     Collection.defaultDelegate(delegate);
@@ -249,7 +246,7 @@ angular.module('MarsDB', []).provider('$collection', function () {
 });
 
 module.export = 'MarsDB';
-},{"./AngularCollectionDelegate":1,"angular":undefined,"marsdb":undefined}],4:[function(require,module,exports){
+},{"./AngularCollectionDelegate":1,"./AngularCursorObservable":2,"angular":undefined,"marsdb":undefined}],4:[function(require,module,exports){
 module.exports = require('./dist/index');
 
 },{"./dist/index":3}]},{},[4]);
--- dist/AngularCollectionDelegate.js
+++ dist/AngularCollectionDelegate.js
@@ -11,12 +11,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.createCollectionDelegate = createCollectionDelegate;
 
-var _AngularCursorObservable = require('./AngularCursorObservable');
-
-var _AngularCursorObservable2 = _interopRequireDefault(_AngularCursorObservable);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
